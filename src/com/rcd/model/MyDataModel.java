21cf97317bd937a51c76e1ab0ee68b4a27412310
bystc <598296757@qq.com>
2018-02-10 15:21:07 +0800

    Update MyDataModel.java

--- src/com/rcd/model/MyDataModel.java
+++ src/com/rcd/model/MyDataModel.java
@@ -1,5 +1,6 @@
 package com.rcd.model;
 
+import org.apache.mahout.cf.taste.impl.model.jdbc.ConnectionPoolDataSource;
 import org.apache.mahout.cf.taste.impl.model.jdbc.MySQLJDBCDataModel;
 import org.apache.mahout.cf.taste.model.JDBCDataModel;
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
@@ -7,20 +8,25 @@ import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 public class MyDataModel {
 
 	public static JDBCDataModel myDataModel() {
-		MysqlDataSource dataSource = new MysqlDataSource();
-		JDBCDataModel dataModel = null;
-		try {
-			dataSource.setServerName("127.0.0.1");
-			dataSource.setUser("root");
-			dataSource.setPassword("hzb19961010");
-			dataSource.setDatabaseName("movie");
-			// use JNDI
-			dataModel = new MySQLJDBCDataModel(DataBaseUtil.getDataSource(),"movie_preferences", "userID", "movieID","preference");
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}
-		return dataModel;
-	}
+        MysqlDataSource dataSource = new MysqlDataSource();
+        JDBCDataModel dataModel = null;
+        try {
+            dataSource.setServerName("localhost");
+            dataSource.setUser("root");
+            dataSource.setPassword("hzb19961010");
+            dataSource.setDatabaseName("movie");
+
+
+            ConnectionPoolDataSource connectionPool=new ConnectionPoolDataSource(dataSource);
+            // use JNDI
+            dataModel = new MySQLJDBCDataModel(connectionPool,"movie_preferences", "userID", "movieID","preference");
+
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return dataModel;
+    } 
 
 }
